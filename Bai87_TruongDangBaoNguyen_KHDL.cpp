3ef19cb084ac09559e97a5e9ea91c03b85e280d0
5ken11 <trdbaonguyen0507@gmail.com>
2025-10-21 16:15:20 +0700

    Update Bai87_TruongDangBaoNguyen_KHDL.cpp

--- Bai87_TruongDangBaoNguyen_KHDL.cpp
+++ Bai87_TruongDangBaoNguyen_KHDL.cpp
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #define max 1001
+
 int A[max + 1][max + 1];
 int B[max + 1][max + 1];
 int m, n;
 int t = 1;
-bool queue(){
+
+bool queue(){                               //Truy vấn xem thử có tiếp tục không?
     char s;
     printf("Tiep tuc?(Y/N):");
-    scanf("%s", &s);
+    scanf("%c", &s);
     if (s == 'Y') return true;
     else if (s == 'N') return false;
 }
-void input(){
+
+void input(){                               //Nhập ma trận ban đầu.
     scanf("%d %d", &m, &n);
     int temp;
     for(int i = 1 ; i <= m; i++){
@@ -21,7 +24,8 @@ void input(){
         }
     }    
 }
-void refresh(){
+
+void refresh(){                             //Làm mới ma trận để chuẩn bị cho truy vấn tiếp theo.
     for(int i = 1 ; i <= m; i++){
         for(int j = 1; j <= n; j++){
            A[i][j] = B[i][j];
@@ -29,30 +33,27 @@ void refresh(){
         }
     }
 }
-int getSum(int i, int j){
+
+int getSum(int i, int j){                   //Tính tổng các cá thể xung quanh vị trí i, j.
     return A[i - 1][j - 1] + A[i - 1][j] + A[i - 1][j + 1] + A[i][j - 1] + A[i][j + 1] + A[i + 1][j - 1] + A[i + 1][j] + A[i + 1][j + 1];    
 }
-void solution(){
+
+void solution(){                            //Phương pháp giải quyết.
     int temp;
     for(int i = 1; i <= m; i++){
         for(int j = 1; j <= n; j++){
             temp = getSum(i, j);
-            if(A[i][j] == 0 && temp == 3){
+            if(A[i][j] == 0 && temp == 3){  //Th1:Nếu có đúng 3 cá thể xung quanh thì sẽ được sinh ra.
                 B[i][j] = 1;
             }
-            else if(temp <= 2 && A[i][j] == 1) B[i][j] = 0;
-            else if(temp > 3 && A[i][j] == 1) B[i][j] = 0;
+            else if(temp < 2 && A[i][j] == 1) B[i][j] = 0; //Th2: Nếu có ít hơn 2 cá thể thì sẽ chết.
+            else if(temp > 3 && A[i][j] == 1) B[i][j] = 0;  //Th3: Nếu có nhiều hơn 3 cá thể thì sẽ chết.
             else B[i][j] = A[i][j];
         }
     }
-    for(int i = 1 ; i <= m; i++){
-        for(int j = 1; j <= n; j++){
-            printf("%d ", B[i][j]);
-        }
-        printf("\n");
-    }
 }
-void printMatrix(){
+
+void printMatrix(){                         //In Ma trận.
     printf("Ma trận sau %d thế hệ:\n", t);
     for(int i = 1 ; i <= m; i++){
         for(int j = 1; j <= n; j++){
@@ -61,6 +62,7 @@ void printMatrix(){
         printf("\n");
     }
 }
+
 int main(){
     input();
     solution();
