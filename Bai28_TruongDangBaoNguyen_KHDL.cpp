d145f054ebec5f8e5ef1cf2def7f8c7b2d0c8761
5ken11 <trdbaonguyen0507@gmail.com>
2025-11-11 21:28:26 +0700

    Update Bai28_TruongDangBaoNguyen_KHDL.cpp

--- Bai28_TruongDangBaoNguyen_KHDL.cpp
+++ Bai28_TruongDangBaoNguyen_KHDL.cpp
@@ -1,32 +1,45 @@
 #include <stdio.h>
 #include <math.h>
-#define max 100005
-int GCD(int a, int b){
-    while (a != b){
-        if (a > b) a -= b;
-        else if (b > a) b -= a;
+#include <stdlib.h>
+long long GCD(long long a, long long b){            //Tính UCLN của 2 số.
+    if (b == 0) return llabs(a);
+    return GCD(b, a % b);
+}
+long long LCM(long long a, long long b){            // Tính BCNN của 2 số.
+    long long ans = GCD(a, b);
+    return llabs(a/ans) * llabs(b);
+}
+int solve(){
+    long long UCLN = 0;
+    long long BCNN = 0;
+    long long n = 0;
+    long long t;
+    while (scanf("%lld", &t) == 1){                   //Đọc số trong dãy cho đến khi hết dữ liệu.
+        if(t == 0) {                                //Nếu đọc được 0 -> Không có UCLN và BCNN
+            printf("Day so khong co UCLN va BCNN.");
+            return 0;
+        }
+        else{
+            t = llabs(t);                           //Lấy giá trị tuyệt đối(Cho trường hợp số âm).
+            if(n == 0){
+                UCLN = t;                           //Khi chỉ đọc giá trị đầu tiên, UCLN và BCNN = chính giá trị đó.
+                BCNN = t;
+            }
+            else{
+                UCLN = GCD(UCLN, t);                //Tính UCLN của n số.
+                BCNN = LCM(BCNN, t);                //Tính BCNN của n số
+            }
+            n++;
+        }
     }
-    return a;
+    if (n == 0) printf("Day so khong co UCLN va BCNN.");               //In kết quả.
+    else{
+        printf("Uoc chung lon nhat cua day so la: %lld\n", UCLN);
+        printf("Boi chung nho nhat cua day so la: %lld\n", BCNN);
+    }
+    return 0;
 }
-int LCM(int a, int b){
-    int ans = GCD(a, b);
-    return (a * b)/ans;
-}
-int n;
-int A[max + 1];
 int main(){
-    printf("Nhập só số hạng muốn tìm UCLN và BCNN:");
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++){
-        scanf("%d", &A[i]);
-    }
-    int UCLN = GCD(A[0], A[1]);
-    int BCNN = LCM(A[0], A[1]);
-    for(int i = 2; i < n; i++){
-        UCLN = GCD(UCLN, A[i]);
-        BCNN = LCM(BCNN, A[i]);
-    }
-    printf("Uoc chung lon nhat cua day so la: %d\n", UCLN);
-    printf("Boi chung nho nhat cua day so la: %d\n", BCNN);
-    
+    solve();
+    return 0;
 }
