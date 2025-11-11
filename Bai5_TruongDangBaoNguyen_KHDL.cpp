5459cc225a656e0f529aa2a38f532417ab746053
5ken11 <trdbaonguyen0507@gmail.com>
2025-11-11 21:34:24 +0700

    Update Bai5_TruongDangBaoNguyen_KHDL.cpp

--- Bai5_TruongDangBaoNguyen_KHDL.cpp
+++ Bai5_TruongDangBaoNguyen_KHDL.cpp
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 int t1, t2;
-int UCLN(int a, int b){
-    if (b == 0) return a;
-    return UCLN(b, a % b);
+int UCLN(int a, int b){                             //Tính UCLN của 2 số theo thuật toán Euclid.
+    while(b != 0){
+        int temp;
+        temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
 }
-int solve(int a, int b){
-    int ans = UCLN(a, b);
+int solve(int a, int b){                               
+    int ans = UCLN(a, b);       
     int t3;
-    while (scanf("%d", &t3) == 1){
-        if(t3 == 0){
+    while (scanf("%d", &t3) == 1){                 //Đọc giá trị của dữ liệu cho tới khi hết dữ liệu.
+        if(t3 == 0){                               //Nếu giá trị đọc được bằng 0 thì không có UCLN.
             printf("Không có UCLN");
             return;
         }
-        ans = UCLN(ans, t3);
+        ans = UCLN(ans, t3);                       //Tính UCLN theo công thức UCLN(a1, a2, a3, ..., an) = UCLN(UCLN(a1, a2, a3, ..., an-1), an);
     }
-    return ans;
+    return ans;                                  
 }
 int main(){
     scanf("%d %d", &t1, &t2);
@@ -25,3 +30,4 @@ int main(){
     }
     else printf("%d", solve(t1, t2));
 }
+
