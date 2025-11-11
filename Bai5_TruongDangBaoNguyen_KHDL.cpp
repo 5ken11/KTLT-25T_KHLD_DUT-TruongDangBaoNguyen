a4b3ee5333f406ba873ac2438b05cffb05b4c17b
5ken11 <trdbaonguyen0507@gmail.com>
2025-11-11 21:00:23 +0700

    Update Bai5_TruongDangBaoNguyen_KHDL.cpp

--- Bai5_TruongDangBaoNguyen_KHDL.cpp
+++ Bai5_TruongDangBaoNguyen_KHDL.cpp
@@ -2,22 +2,26 @@
 #include <math.h>
 int t1, t2;
 int UCLN(int a, int b){
-    while (a != b){
-        if (a > b) a -= b;
-        else if (b > a) b -= a;
-    }
-    return a;
+    if (b == 0) return a;
+    return UCLN(b, a % b);
 }
 int solve(int a, int b){
     int ans = UCLN(a, b);
     int t3;
-    scanf("%d", &t3);
-    if(t3 != 0){
-        return UCLN(ans, t3);
+    while (scanf("%d", &t3) == 1){
+        if(t3 == 0){
+            printf("Không có UCLN");
+            return;
+        }
+        ans = UCLN(ans, t3);
     }
-    else return ans;
+    return ans;
 }
 int main(){
     scanf("%d %d", &t1, &t2);
-    printf("%d", solve(t1, t2));
+    if(t1 == 0 || t2 == 0){
+        printf("Không có UCLN");
+        return 0;
+    }
+    else printf("%d", solve(t1, t2));
 }
