897c674f90670b5ecb2f0dcf6e8a483ec4a421ef
5ken11 <trdbaonguyen0507@gmail.com>
2025-11-11 21:30:18 +0700

    Update Bai20_TruongDangBaoNguyen_KHDL.cpp

--- Bai20_TruongDangBaoNguyen_KHDL.cpp
+++ Bai20_TruongDangBaoNguyen_KHDL.cpp
@@ -3,18 +3,19 @@
 
 int n;
 bool check(int n){
-    int ans = n*n;
+    int ans = n*n;                              //Tính bình phương của số cho trước.
     int result = 0;
     while(n > 0){
-        int temp = n % 10;
+        int temp = n % 10;                      //Lấy từng chữ số của số cho trước.
         n = n / 10;
-        result += temp*temp*temp;
+        result += temp*temp*temp;               //Tổng của các chữ số của số cho trước.
     }
-    if(ans == result) return true;
-    else return false;
+    if(ans == result) return true;              //So sánh và trả về kết quả.
+    else return false;                          
 }
 int main(){
     scanf("%d", &n);
-    if(check(n)) printf("n^2 bằng tổng các lập phương các chữ số của n");
+    if(check(n)) printf("n^2 bằng tổng các lập phương các chữ số của n");           //In kết quả.
     else printf("n^2 không bằng tổng các lập phương các chữ số của n");
+
 }
